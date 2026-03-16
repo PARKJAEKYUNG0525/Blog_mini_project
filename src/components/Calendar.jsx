3f63c17ed951d30a0b3fc0d580385236e4646dd0
PARKJAEKYUNG0525 <pjg0525@naver.com>
2026-03-16 21:35:08 +0900

    수저ㅗㅇ

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -4,7 +4,6 @@ const Calendar = () => {
     return (
         <div className="flex justify-center items-center h-screen text-3xl font-bold">
             캘린더
-            <MyCalendar />
         </div>
     );
 };
