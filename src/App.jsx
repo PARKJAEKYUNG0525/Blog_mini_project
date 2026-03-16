8863373396e39de0a40709622cab1897caa56a74
Jin <koreanjinjin2@gmail.com>
2026-03-16 20:00:34 +0900

    navi edit hj

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import MemberList from "./components/MemberList";
 import EditBoard from "./components/EditBoard";
+import MyBoard from './components/MyBoard';
+import Calender from './components/Calender';
 
 const App = () => {
   return (
@@ -23,6 +25,8 @@ const App = () => {
           <Route path='/memberList' element={<MemberList />}></Route>
           <Route path='/board/create' element={<CreateBoard />}></Route>
           <Route path='/board/edit/:id' element={<EditBoard />}></Route>
+          <Route path='/MyBoard' element={<MyBoard />}></Route>
+          <Route path='/Calender' element={<Calender />}></Route>
         </Routes>
       </BrowserRouter>
     </AuthContextPro>
