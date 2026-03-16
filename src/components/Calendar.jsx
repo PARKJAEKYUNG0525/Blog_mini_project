f4a739a57386c6d027d116d9a01c30a7c5536a4d
gingy2 <hjin9412@naver.com>
2026-03-16 20:34:35 +0900

    Create Calendar.jsx

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Calendar = () => {
+    return (
+        <div className="flex justify-center items-center h-screen text-3xl font-bold">
+            캘린더
+        </div>
+    );
+};
+
+export default Calendar;
