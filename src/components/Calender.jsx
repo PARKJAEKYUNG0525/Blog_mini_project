ad802ae6463f6a43d9d0e7f4f1912c2a065e3d67
enkyong <diw119dks@naver.com>
2026-03-16 21:38:59 +0900

    edit calendar

--- src/components/Calender.jsx
+++ src/components/Calender.jsx
@@ -3,8 +3,7 @@ import MyCalendar from '../CalendarPage/MyCalendar';
 
 const Calender = () => {
     return (
-        <div className="flex justify-center items-center h-screen text-3xl font-bold">
-            캘린더
+        <div className="flex justify-center items-center h-screen text-xl font-bold">
             <MyCalendar />
         </div>
     );
