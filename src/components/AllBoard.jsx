3876bae228564d8da25ff63aebddf247d2463503
gingy2 <hjin9412@naver.com>
2026-03-16 20:34:12 +0900

    Create AllBoard.jsx

---
+++ src/components/AllBoard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllBoard = () => {
+    return (
+        <div className="flex justify-center items-center h-screen text-3xl font-bold">
+            전체 게시글
+        </div>
+    );
+};
+
+export default AllBoard;
